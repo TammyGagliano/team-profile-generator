0effa4aa5ea4d94cd0dc191460ea88028a5a4789
Tammy Gagliano <tracsd@gmail.com>
2021-10-14 21:45:16 -0500

    begin work on html and css creating cards

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-//Testing to make sure works
 
-const Employee = require('./lib/Employee');
-
-const e2 = new Employee("Jack", 1, "a@a.com");
-console.log("name: ", e2.getName());
-console.log("e2 id: ", e2.getId());
-console.log("e2 email: ", e2.getEmail());
+const Engineer = require("../lib/Engineer");
+const Manager = require("../lib/Manager");
+const Intern = require("../lib/Intern.js");
+const inquirer = require("inquirer");
