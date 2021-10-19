c7e6387b076de350b3292893a67d3705cf720a76
Tammy Gagliano <tracsd@gmail.com>
2021-10-18 20:33:04 -0500

    create html and css files to generate content

---
+++ dist/generateHTML.js
@@ -0,0 +1,54 @@
+/*
+  generateHTML: return html markup in the form of a string
+  generateCSS: include css rules in the form of a string that we will inject in this file
+*/
+
+/*
+  renderCards: given a data(can be an engineer, intern, manager, employee), generate HTML that consist of 
+  a card
+*/
+function renderCards(data) {
+    return `<div class="card">
+    <div class="card-body">
+        <h4 class="card-title">${data.name}</h4>
+        <h6 class="text-muted card-sub-title mb-2">${data.role}</h6>
+        <p>${data.id}</p>
+        <p>${data.email}</p>
+        <p>${data.officenumber}</p>
+    </div>
+    </div>`;
+}
+
+/*
+ generateHTML: returns the html that index.js will eventually use to inject into the DOM
+*/
+function generateHtml(data) {
+    const cards = [];
+    for (let i=0; i < data.length; i++) {
+        const cardSection = renderCards(data[i]);
+        cards.push(cardSection);
+    }
+
+    return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="./styles.css"/>
+    <title>Team Profile Generator</title>
+</head>
+<body>
+    <header class="d-lg-flex justify-content-lg-center align-items-lg-center">
+        <h2>My Team</h2>
+    </header>
+    <div class="container-fluid d-flex flex-row justify-content-center flex-wrap">
+        ${cards.join('\n')}
+    </div>
+    <script src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>`;
+}
+
+module.exports = generateHtml;
--- index.js
+++ index.js
@@ -3,3 +3,107 @@ const Engineer = require("../lib/Engineer");
 const Manager = require("../lib/Manager");
 const Intern = require("../lib/Intern.js");
 const inquirer = require("inquirer");
+const fs = require("fs");
+// const generateHTML
+// generateCSS
+
+
+function (fileName, data) {
+    fs.writeFileSync(fileName), data);
+    err ? console.log(err) : console.log("Successfully created HTML file!")
+  }
+  
+  function engineerCard {
+  inquirer prompt ([
+    {
+      type: "input",
+      message: "What is your name?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What is your id?",
+      name: "ID",
+    },
+    {
+      type: "input",
+      message: "What school do you attend?",
+      name: "school",
+    },
+    {
+      type: "...",
+      message: "Would you like add another team member?",
+      choices: ["Engineer", "Intern", "Quit"],
+      name: "member"
+    },
+  ])
+}
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="col-12 text-center">
+            <h2>My Team</h2>
+        </div>
+    </header>
+
+    <div class="container-fluid">
+    <div class="row">
+
+        <div class="col-sm-6 col-md-4">
+            <div class="card border-white">
+                <div class="card-header">Heading 1</div>
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build 
+                           on the card 
+                        title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        
+
+        <div class="col-sm-6 col-md-4">
+            <div class="card border-white">
+                <div class="card-header">Heading 2</div>
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build 
+                          on the card 
+                        title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+    
+
+        <div class="col-sm-6 col-md-4">
+            <div class="card border-white">
+                <div class="card-header">Heading 3</div>
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build 
+                         on the card 
+                        title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        
+
+
+    
+</body>
+</html>
+
+
+
+
+
+
+
