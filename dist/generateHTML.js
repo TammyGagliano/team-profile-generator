0a01d66b72b34ba87fd30c4dfc903e3f3411aebd
Tammy Gagliano <tracsd@gmail.com>
2021-10-18 22:19:37 -0500

    udpate files by creating individual profiles within files.

--- dist/generateHTML.js
+++ dist/generateHTML.js
@@ -1,54 +1,70 @@
-/*
-  generateHTML: return html markup in the form of a string
-  generateCSS: include css rules in the form of a string that we will inject in this file
-*/
+// create profile of team members
 
-/*
-  renderCards: given a data(can be an engineer, intern, manager, employee), generate HTML that consist of 
-  a card
-*/
-function renderCards(data) {
-    console.log('data: ', data);
-    return `<div class="card">
-    <div class="card-body">
-        <h4 class="card-title">${data.name}</h4>
-        <h6 class="text-muted card-sub-title mb-2">${data.role}</h6>
-        <p>${data.id}</p>
-        <p>${data.email}</p>
-        <p>${data.officenumber}</p>
-    </div>
-    </div>`;
+const createProfile = team => {
+
+
+const createManager = manager => {
+  return `
+  <div class="card employee-card manager-card">
+            <div class="card-header text-center">
+                <h2 class="card-title">${manager.getName()}</h2>
+                <h4 class="card-title">Title: ${manager.getRole()}</h4>
+            </div>
+            <div class="card-body bg-light">
+                <ul class="list-group text-dark">
+                    <li class="list-group-item">ID: ${manager.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                    <li class="list-group-item">Office number: <a href="tel:${manager.getOfficeNumber()}">${manager.getOfficeNumber()}</a></li>
+                </ul>
+            </div>
+        </div>
+        `;
+    };
+
 }
+ // Create Engineer Profile
 
-/*
- generateHTML: returns the html that index.js will eventually use to inject into the DOM
-*/
+ 
+ // Create Intern Profile
+
+ //generateHTML: returns the html that index.js will eventually use to inject into the DOM
+
 function generateHtml(data) {
-    const cards = [];
-    for (let i=0; i < data.length; i++) {
-        const cardSection = renderCards(data[i]);
-        cards.push(cardSection);
-    }
-    return `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="styles.css"/>
+  const cards = [];
+  for (let i = 0; i < data.length; i++) {
+    const cardSection = renderCards(data[i]);
+    cards.push(cardSection);
+  }
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>Team Profile Generator</title>
-</head>
-<body>
-    <header class="d-lg-flex justify-content-lg-center align-items-lg-center">
-        <h2>My Team</h2>
-    </header>
-    <div class="container-fluid d-flex flex-row justify-content-center flex-wrap">
-        ${cards.join('\n')}
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="styles.css"/>
+        <title>Team Profile Generator</title>
+    </head>
+    <body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 jumbotron text-white mb-3">
+                <h1 class="text-center">My Team</h1>
+            </div>
+        </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
-</body>
-</html>`;
-}
+    <div class="container">
+        <div class="row">
+            <div class="main-section col-12 d-flex justify-content-center">
+            ${createProfile(team)}
+            </div>
+        </div>
+    </div>    
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+    </body>
+    </html>`;
+    }
 
 module.exports = generateHtml;
--- index.js
+++ index.js
@@ -1,168 +1,161 @@
+// Dependencies
 const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
+const jest = require("jest");
+
+// Constructors
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern.js");
 const generateHtml = require("./dist/generateHtml.js");
 const generateCss = require("./dist/generateCss.js");
-const employees = [];
 
-function writeToFile(fileName, data) {
-    const htmlContent = generateHtml(data);
+// Create empty arrays for teams as place holders
+const employees = [];
+const team = [];
+
+function init() {
+
+function addManager() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is the Manager's name?",
+                name: "managerName"
+            },
+            {
+                type: "input",
+                message: "What is the Manager's id?",
+                name: "managerId"
+            },
+            {
+                type: "input",
+                message: "What is the Manager's e-mail address?",
+                name: "managerEmail"
+            },
+            {
+                type: "input",
+                message: "What is the Manager's office number?",
+                name: "managerOfficeNumber"
+            }
+        ])
+        .then((res) => {
+            const manager = new Manager(res.managerName, res.managerId, res.managerEmail, managerOfficeNumber);
+            employees.push(manager);
+            team.push(res.managerId);
+            addTeam();
+        });
+}
+
+function addTeam() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "memberChoice",
+            message: "What would you like to add next?",
+            choices: [
+                "Engineer",
+                "Intern",
+                "End application"
+            ]
+        }
+    ]).then(userChoice => {
+        switch (userChoice.memberChoice) {
+            case "Engineer":
+                addEngineer();
+                break;
+            case "Intern":
+                addIntern();
+                break;
+            default:
+                generateHTML();
+        }
+    });
+}
+
+
+function addEngineer() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is the engineer's name?",
+                name: "engineerName"
+            },
+            {
+                type: "input",
+                message: "What is the engineer's id?",
+                name: "engineerId"
+            },
+            {
+                type: "input",
+                message: "What is the engineer's e-mail address?",
+                name: "engineerEmail"
+            },
+            {
+                type: "input",
+                message: "What is the engineer's github username?",
+                name: "engineerGithub"
+            }
+        ])
+        .then((res) => {
+            const engineer = new Engineer(res.engineerName, res.engineerId, res.engineerEmail, res.engineer.Github);
+            employees.push(engineer);
+            team.push(res.engineerId);
+            addTeam();
+        });
+}
+
+
+function addIntern() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is the intern's name?",
+                name: "internName"
+            },
+            {
+                type: "input",
+                message: "What the intern's id?",
+                name: "internId"
+            },
+            {
+                type: "input",
+                message: "What is the intern's e-mail address?",
+                name: "internEmail"
+            },
+            {
+                type: "input",
+                message: "What school did the intern attend?",
+                name: "internSchool"
+            }
+        ])
+        .then((res) => {
+            const intern = new Intern(res.internName, res.internId, res.internEmail, res.internSchool);
+            employees.push(intern);
+            team.push(res.internId);
+            addTeam();
+        });
+}
+
+    function writeToFile(fileName, data) {
+    const htmlContent = generateHTML(data);
 
     fs.writeFile(path.join(process.cwd(),fileName), htmlContent, (err) =>
     err ? console.log(err) : console.log("Successfully created HTML file!")
     );
 
-    const cssContent = generateCss();
+    const cssContent = generateCSS();
 
     fs.writeFile(path.join(process.cwd(), "styles.css"), cssContent, (err) => 
-    err ? console.log(err) : console.log("Succesfully created CSS file!")
+    err ? console.log(err) : console.log("Successfully created CSS file!")
     );
 }
-
-function init() {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                message: "What is your name?",
-                name: "name"
-            },
-            {
-                type: "input",
-                message: "What is your id?",
-                name: "id"
-            },
-            {
-                type: "input",
-                message: "What is your e-mail address?",
-                name: "email"
-            },
-            {
-                type: "input",
-                message: "What is your office number?",
-                name: "officenumber"
-            },
-            {
-                type: "rawlist",
-                message: "Would you like add another team member?",
-                choices: ["Engineer", "Intern", "Quit"],
-                name: "choices"
-            }
-        ])
-        .then((res) => {
-            const { name, id, email, officenumber } = res;
-
-            const manager = new Manager(name, id, email, officenumber);
-
-            employees.push(manager);
-
-            switch (res.choices) {
-                case 'Engineer':
-                    return engineerCard();
-                case 'Intern':
-                    return internCard();
-                case 'Quit':
-                    return writeToFile("index.html", employees);
-            }
-        });
+    addManager();
 }
-
-function engineerCard() {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                message: "What is your name?",
-                name: "name"
-            },
-            {
-                type: "input",
-                message: "What is your id?",
-                name: "id"
-            },
-            {
-                type: "input",
-                message: "What is your e-mail address?",
-                name: "email"
-            },
-            {
-                type: "input",
-                message: "What is your github username?",
-                name: "github"
-            },
-            {
-                type: "rawlist",
-                message: "Would you like add another team member?",
-                choices: ["Engineer", "Intern", "Quit"],
-                name: "choices"
-            }
-        ])
-        .then((res) => {
-            const engineer = new Engineer(res);
-
-            employees.push(engineer);
-
-            switch (res.choices) {
-                case 'Engineer':
-                    return engineerCard();
-                case 'Intern':
-                    return internCard();
-                case 'Quit':
-                    return writeToFile("index.html", employees);
-            }
-        });
-}
-
-
-function internCard() {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                message: "What is your name?",
-                name: "name"
-            },
-            {
-                type: "input",
-                message: "What is your id?",
-                name: "id"
-            },
-            {
-                type: "input",
-                message: "What is your e-mail address?",
-                name: "email"
-            },
-            {
-                type: "input",
-                message: "What school do you attend?",
-                name: "school"
-            },
-            {
-                type: "rawlist",
-                message: "Would you like add another team member?",
-                choices: ["Engineer", "Intern", "Quit"],
-                name: "choices"
-            }
-        ])
-        .then((res) => {
-            const intern = new Intern(res);
-            
-            employees.push(intern);
-
-            switch (res.choices) {
-                case 'Engineer':
-                    return engineerCard();
-                case 'Intern':
-                    return internCard();
-                case 'Quit':
-                    return writeToFile("index.html", employees);
-            }
-        });
-}
-
 init();
 
 
